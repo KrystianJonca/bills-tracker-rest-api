f702855e443916c83b7959a0488459860d36c694
Krystian <krystianjonca17@gmail.com>
2022-06-11 20:43:30 +0200

    Added config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BillModule } from './bill/bill.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BillModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    AuthModule,
+    UserModule,
+    BillModule,
+  ],
 })
 export class AppModule {}
