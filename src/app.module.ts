d672c27aead4b2e76e9874acbddfa027ff6d7f17
Krystian <krystianjonca17@gmail.com>
2022-06-11 19:09:47 +0200

    Generated modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BillModule } from './bill/bill.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BillModule],
 })
 export class AppModule {}
