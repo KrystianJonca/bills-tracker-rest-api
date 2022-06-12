35df6b0917dab43ba006d9dc0a348b517287d4b8
Krystian <krystianjonca17@gmail.com>
2022-06-12 22:52:47 +0200

    Added edit and delete patchs

---
+++ src/user/user.controller.ts
@@ -0,0 +1,34 @@
+import {
+  Controller,
+  Get,
+  Delete,
+  UseGuards,
+  Patch,
+  Body,
+} from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
+import { UserService } from './user.service';
+import { EditUserDto } from './dto';
+
+@UseGuards(JwtGuard)
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getUser(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: User['id'], @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
+
+  @Delete()
+  deleteUser(@GetUser('id') userId: User['id']) {
+    return this.userService.deleteUserById(userId);
+  }
+}
