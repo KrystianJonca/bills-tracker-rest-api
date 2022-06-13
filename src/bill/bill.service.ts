fca4a978655e76af96d058e72c7e8d5fb9b29f4c
Krystian <krystianjonca17@gmail.com>
2022-06-13 12:52:14 +0200

    Added business logic

--- src/bill/bill.service.ts
+++ src/bill/bill.service.ts
@@ -1,7 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Bill, User } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
+import { CreateBillDto, EditBillDto } from './dto';
 
 @Injectable()
 export class BillService {
   constructor(private readonly db: DatabaseService) {}
+
+  async getAllBills(userId: User['id']) {
+    const bills = await this.db.bill.findMany({ where: { userId } });
+    return bills;
+  }
+
+  async getBill(userId: User['id'], billId: Bill['id']) {
+    const bill = await this.db.bill.findFirst({
+      where: {
+        id: billId,
+        userId,
+      },
+    });
+    return bill;
+  }
+
+  async createBill(userId: User['id'], dto: CreateBillDto) {
+    const newBill = await this.db.bill.create({ data: { userId, ...dto } });
+    return newBill;
+  }
+
+  async editBill(userId: User['id'], billId: Bill['id'], dto: EditBillDto) {
+    const bill = await this.db.bill.findUnique({ where: { id: billId } });
+
+    if (!bill || bill.userId !== userId)
+      throw new ForbiddenException('Resourse is not accessible.');
+
+    const editedBill = await this.db.bill.update({
+      where: { id: billId },
+      data: { ...dto },
+    });
+    return editedBill;
+  }
+
+  async deleteBill(userId: User['id'], billId: Bill['id']) {
+    const bill = await this.db.bill.findUnique({ where: { id: billId } });
+
+    if (!bill || bill.userId !== userId)
+      throw new ForbiddenException('Resourse is not accessible.');
+
+    const deletedBill = await this.db.bill.delete({ where: { id: billId } });
+    return deletedBill;
+  }
 }
