13d9a9660bd966498e6aaa368e56bcece2dd662e
Krystian <krystianjonca17@gmail.com>
2022-06-13 18:05:40 +0200

    Created test todos

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,16 +1,76 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './../src/app.module';
+import { DatabaseService } from '../src/database/database.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let db: DatabaseService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen('3000');
+    db = app.get(DatabaseService);
+    await db.cleanDb();
+  });
+
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Auth', () => {
+    describe('Signin', () => {
+      it.todo('Should sign in');
+    });
+
+    describe('Signup', () => {
+      it.todo('Should sign up');
+    });
+  });
+
+  describe('User', () => {
+    describe('Get user', () => {
+      it.todo('Should get the user');
+    });
+
+    describe('Edit user', () => {
+      it.todo('Should edit the user');
+    });
+
+    describe('Delete user', () => {
+      it.todo('Should delete the user');
+    });
+  });
+
+  describe('Bills', () => {
+    describe('Get bill', () => {
+      it.todo('Should get the bill');
+    });
+
+    describe('Get all bills', () => {
+      it.todo('Should get all the bills');
+    });
+
+    describe('Create new bill', () => {
+      it.todo('Should create a new bill');
+    });
+
+    describe('Edit bill', () => {
+      it.todo('Should edit the bill');
+    });
+
+    describe('Delete bill', () => {
+      it.todo('Should delete the bill');
+    });
   });
 });
