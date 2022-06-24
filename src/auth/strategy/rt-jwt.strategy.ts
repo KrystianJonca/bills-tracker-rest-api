b352be5bbb4a815258d8cc46a2d13e1c4f913a94
KrystianJonca <krystianjonca43@gmail.com>
2022-06-24 18:50:34 +0200

    Removed unused variable

--- src/auth/strategy/rt-jwt.strategy.ts
+++ src/auth/strategy/rt-jwt.strategy.ts
@@ -3,22 +3,17 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { DatabaseService } from '../../database/database.service';
-import { Request } from 'express';
 
 @Injectable()
 export class RtJwtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor(config: ConfigService, private readonly db: DatabaseService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: config.get<string>('RT_JWT_SECRET'),
-      passReqToCallback: true,
+      secretOrKey: config.get('RT_JWT_SECRET'),
     });
   }
 
-  async validate(
-    req: Request,
-    payload: { sub: number; email: string; username: string },
-  ) {
+  async validate(payload: { sub: number; email: string; username: string }) {
     const user = await this.db.user.findUnique({ where: { id: payload.sub } });
     delete user.passwordHash;
     return user;
